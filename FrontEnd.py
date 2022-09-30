4a850abc06be4e4a302371cdee0384ad034d43de
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-09-30 15:35:06 +0200

    Partie 3 et parti4 liée

--- FrontEnd.py
+++ FrontEnd.py
@@ -105,8 +105,6 @@ def partie1():
         width = 396,
         height = 40)
 
-
-
     lClasse = Label(partie1)
     lClasse.place(
         x = 350, y = 315,
@@ -288,6 +286,262 @@ def partie2():
     partie2.resizable(False, False)
     partie2.mainloop()
 
+def partie3():
+    def btnAccueil():
+        partie3.destroy()
+        accueil()
+        
+    def btnCalcul():
+        if(AnalyseIP.validationIP(entry0.get())):
+            if(AnalyseIP.validationMasque(entry1.get())):
+                if(AnalyseIP.validationIP(entry2.get())):
+                    if(AnalyseIP.verifEgaliteAdresse(entry0.get(), entry1.get(), entry2.get())):
+                        lResult.config(text="Cette Ip fait partie de ce réseau")
+                    else:
+                        lResult.config(text="Cette Ip ne fait pas partie de ce réseau")
+                else:
+                    lResult.config(text="Ip réseau invalide")
+            else:
+                lResult.config(text="Masque invalide")
+        else: 
+            lResult.config(text="IP invalide")
+    
+    partie3 = Tk()
+    partie3.geometry("1000x600")
+    partie3.configure(bg = "#ffffff")
+    canvas = Canvas(
+        partie3,
+        bg = "#ffffff",
+        height = 600,
+        width = 1000,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)
+
+    background_img = PhotoImage(file = f"./images/partie3/background.png")
+    background = canvas.create_image(
+        500.0, 300.0,
+        image=background_img)
+
+    entry0_img = PhotoImage(file = f"./images/partie3/img_textBox0.png")
+    entry0_bg = canvas.create_image(
+        500.0, 231.0,
+        image = entry0_img)
+
+    lResult = Label(partie3, text="")
+    lResult.place(
+        x = 400, y = 430,
+        width = 300,
+        height = 30)
+    lResult['background'] = colorBack
+
+    entry0 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry0.place(
+        x = 302, y = 210,
+        width = 396,
+        height = 40)
+
+    entry1_img = PhotoImage(file = f"./images/partie3/img_textBox1.png")
+    entry1_bg = canvas.create_image(
+        500.0, 295.0,
+        image = entry1_img)
+
+    entry1 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry1.place(
+        x = 302, y = 274,
+        width = 396,
+        height = 40)
+
+    entry2_img = PhotoImage(file = f"./images/partie3/img_textBox2.png")
+    entry2_bg = canvas.create_image(
+        500.0, 358.0,
+        image = entry2_img)
+
+    entry2 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry2.place(
+        x = 302, y = 337,
+        width = 396,
+        height = 40)
+
+    img0 = PhotoImage(file = f"./images/partie3/img0.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btnCalcul,
+        relief = "flat")
+
+    b0.place(
+        x = 139, y = 420,
+        width = 191,
+        height = 42)
+
+    img1 = PhotoImage(file = f"./images/partie3/img1.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btnAccueil,
+        relief = "flat")
+
+    b1.place(
+        x = 93, y = 91,
+        width = 118,
+        height = 109)
+
+    partie3.resizable(False, False)
+    partie3.mainloop()
+
+def partie4():
+    def btnAccueil():
+        partie4.destroy()
+        accueil()
+    
+    def btnCalcul():
+        if(AnalyseIP.validationIP(entry0.get())):
+            if(AnalyseIP.validationMasque(entry1.get())):
+                if(AnalyseIP.validationIP(entry2.get())):
+                    if(AnalyseIP.validationMasque(entry3.get())):
+                        if(AnalyseIP.verifEgaliteAdresseDeuxIp(entry0.get(), entry1.get(), entry2.get(), entry3.get())):
+                              lResult.config(text="Les deux machines considère l'autre comme faisant partie de son réseau")
+                        else:
+                            lResult.config(text="Les deux machines ne considère pas l'autre comme faisant partie de son réseau")
+                    else:
+                        lResult.config(text="Le masque 2 est invalide")
+                else:
+                    lResult.config(text="L'IP 2 est invalide")
+            else:
+                lResult.config(text="Le masque 1 est invalide")
+        else: 
+            lResult.config(text="L'IP 1 est invalide")
+    
+    partie4 = Tk()
+
+    partie4.geometry("1000x600")
+    partie4.configure(bg = "#ffffff")
+    canvas = Canvas(
+        partie4,
+        bg = "#ffffff",
+        height = 600,
+        width = 1000,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)
+
+    background_img = PhotoImage(file = f"./images/partie4/background.png")
+    background = canvas.create_image(
+        500.0, 300.0,
+        image=background_img)
+
+    entry0_img = PhotoImage(file = f"./images/partie4/img_textBox0.png")
+    entry0_bg = canvas.create_image(
+        480.0, 239.0,
+        image = entry0_img)
+
+    lResult = Label(partie4,text="")
+    lResult.place(
+        x = 250, y = 459,
+        width = 500,
+        height = 30)
+    lResult['background'] = colorBack
+
+    entry0 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry0.place(
+        x = 282, y = 218,
+        width = 396,
+        height = 40)
+
+    img0 = PhotoImage(file = f"./images/partie4/img0.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btnCalcul,
+        relief = "flat")
+
+    b0.place(
+        x = 707, y = 298,
+        width = 191,
+        height = 42)
+
+    img1 = PhotoImage(file = f"./images/partie4/img1.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btnAccueil,
+        relief = "flat")
+
+    b1.place(
+        x = 93, y = 91,
+        width = 118,
+        height = 109)
+
+    entry1_img = PhotoImage(file = f"./images/partie4/img_textBox1.png")
+    entry1_bg = canvas.create_image(
+        480.0, 291.0,
+        image = entry1_img)
+
+    entry1 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry1.place(
+        x = 282, y = 270,
+        width = 396,
+        height = 40)
+
+    entry2_img = PhotoImage(file = f"./images/partie4/img_textBox2.png")
+    entry2_bg = canvas.create_image(
+        480.0, 361.0,
+        image = entry2_img)
+
+    entry2 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry2.place(
+        x = 282, y = 340,
+        width = 396,
+        height = 40)
+
+    entry3_img = PhotoImage(file = f"./images/partie4/img_textBox3.png")
+    entry3_bg = canvas.create_image(
+        479.0, 413.0,
+        image = entry3_img)
+
+    entry3 = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    entry3.place(
+        x = 281, y = 392,
+        width = 396,
+        height = 40)
+
+    partie4.resizable(False, False)
+    partie4.mainloop()
     
 # MAIN
 
@@ -300,6 +554,14 @@ def accueil():
     def btnParti2():
         accueil.destroy()
         partie2()
+    
+    def btnParti3():
+        accueil.destroy()
+        partie3()
+    
+    def btnParti4():
+        accueil.destroy()
+        partie4()
         
     accueil = Tk()
     accueil.geometry("1000x600")
@@ -350,7 +612,7 @@ def accueil():
         image = img2,
         borderwidth = 0,
         highlightthickness = 0,
-        command = btnParti1,
+        command = btnParti4,
         relief = "flat")
 
     b2.place(
@@ -376,7 +638,7 @@ def accueil():
         image = img4,
         borderwidth = 0,
         highlightthickness = 0,
-        command = btnParti1,
+        command = btnParti3,
         relief = "flat")
 
     b4.place(
